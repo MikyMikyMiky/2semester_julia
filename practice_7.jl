e32edb211d27d13a569cf0aafd6aaf79a16fed2a
MikyMikyMiky <mike1024b@mail.ru>
2021-05-09 11:43:00 +0300

    Update practice_7.jl

--- practice_7.jl
+++ practice_7.jl
@@ -182,6 +182,19 @@ function allnilpotents(n::Integer)
     return nil
 end
 
+#Задача 12
+function order(a,p)
+    phi = p-1
+    res = 0
+    for i in 1:(p-1)
+        if (phi%i==0 && (a^i)%p==1)
+            res = i
+            break
+        end
+    end
+    return res
+end
+
 #Задача 13
 function bisect(f::Function, a, b, ε)
     y_a=f(a)
