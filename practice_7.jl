cc1e734a417f8f092ac1538b76a1c27000338258
MikyMikyMiky <mike1024b@mail.ru>
2021-05-09 15:31:21 +0300

    Все выполнено

--- practice_7.jl
+++ practice_7.jl
@@ -68,10 +68,6 @@ function eratosphen(n::Integer)
     ser=fill(true,n)
     ser[1]=false
     k=2
-    #=ИНВАРИАНТ: 
-    - число k - простое
-    - все элементы в ser[1:k] c простыми индексами, и только они, имеют значение true
-    =#
     while k<n || k !== nothing
         ser[2k:k:end] .= false # но лучше: numser[k^2:k:end] .= false
         k=findnext(ser, k+1)
