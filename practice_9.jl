061e02228c8e0c88fabb8703fa27f1e86e0ea95b
MikyMikyMiky <mike1024b@mail.ru>
2021-05-09 20:46:21 +0300

    В процессе

--- practice_9.jl
+++ practice_9.jl
@@ -30,7 +30,16 @@ end
 
 #Задача 3
 
-#Сделаю
+function build_cos_plots()
+    f1(x) = 1-(x^2)/2
+    f2(x) = f1(x) + (x^4)/factorial(4)
+    f3(x) = f2(x) - (x^6)/factorial(6) + (x^8)/factorial(8)
+    f4(x) = f3(x) - (x^10)/factorial(10) + (x^12)/factorial(12) - (x^14)/factorial(14) + (x^16)/factorial(16)
+    p=plot(f1) # for i in (f1,f2,f3,f4) не работает
+    plot!(f2)
+    plot!(f3)
+    plot!(f4)
+end
 
 #Задача 4
 function ln(x,ε)
@@ -157,10 +166,7 @@ end
 
 #Задача 9
 
-function inv(A)
-    B=copy(A)
-end
-    
+#Сделаю
 
 #Задача 10
 
@@ -176,9 +182,8 @@ end
 
 #Задача 11
 
-function SLAE(Ab)
-    B=copy(Ab)
-    if rang(B)==length(B)
-        return zeros(length(B))
-    end
-end
+#Сделаю
+
+#Задача 12
+
+#Сделаю
