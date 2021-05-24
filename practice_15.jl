a4c86789eb69156a5df8cf798b6e55baead63e38
MikyMikyMiky <mike1024b@mail.ru>
2021-05-24 10:29:15 +0300

    Все выполнено

--- practice_15.jl
+++ practice_15.jl
@@ -179,3 +179,32 @@ function alltypes(type)
         alltypes(i)
     end
 end
+
+#Задача 7
+function find_general(tree::Vector, setver::Set)
+    number_visited = 0
+    general = 0
+    function recurstrace(tree, parent=0)  
+        is_mutable_general = false
+
+        for subtree in tree[begin:end-1]
+            if number_visited < length(setver)
+                recurstrace(subtree, tree[end])
+            end
+        end
+        if tree[end] in setver
+            number_visited +=1
+            if number_visited == 1
+                general = tree[end]
+            end                        
+        end
+        if general==tree[end] 
+            is_mutable_general = true
+        end
+        if is_mutable_general && number_visited < length(setver)
+            general = parent
+        end
+    end
+    recurstrace(tree)
+    return general
+end
